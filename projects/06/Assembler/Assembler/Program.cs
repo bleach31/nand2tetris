1d96c1f51d91ec5109ac58ecaa866cca8dde3e3a
MOTOHIRO SHIBAKAWA <10001176180@dnjp.globaldenso>
2016-05-25 17:29:48 +0900

    Parserまで

--- projects/06/Assembler/Assembler/Program.cs
+++ projects/06/Assembler/Assembler/Program.cs
@@ -10,11 +10,12 @@ namespace Assembler
 	{
 		static void Main(string[] args)
 		{
-			String filepath = @"C:\Users\10001176180\Documents\16FY_教育研修\コンピュータ実装教育\nand2tetris\projects\06\add\Add.asm";
+			String filepath1 = @"C:\Users\10001176180\Documents\16FY_教育研修\コンピュータ実装教育\nand2tetris\projects\06\add\Add.asm";
 			String filepath2 = @"C:\Users\10001176180\Documents\16FY_教育研修\コンピュータ実装教育\nand2tetris\projects\06\max\Max.asm";
 
+			String filepath3 = Console.ReadLine();
 
-			Parser test = new Parser(filepath2);
+			Parser test = new Parser(filepath3);
 			while (test.hasMoreCommands())
 			{
 				Console.WriteLine("{0} \t\t {1},{2},{3},{4},{5}", test.line, test.commandtype, test.symbol, test.comp, test.dest, test.jump);
